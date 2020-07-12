1cc32be69afe94c1e6fa97b9fe02655ef7a30aab
Soares Dolapo <49039946+DolapoSoares@users.noreply.github.com>
2020-07-12 18:34:11 +0100

    Add files via upload

---
+++ challenge.js
@@ -0,0 +1,11 @@
+var sum = Number(window.prompt("Type a number", ""));
+
+for (var x = 0; x < 1000; x++)
+{
+    if (x % 3 === 0 || x % 5 === 0)
+    {
+       sum += x;
+    }
+}
+console.log(sum);
+alert("The total sum is " + sum)
